7a701b08deb32e21b7b024f2f8137e0451074206
Michael-B1764 <mikebailar@gmail.com>
2020-08-05 13:09:11 -0700

    lookup complete. 1/8

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,6 +1,10 @@
 # Your code here
+import math
+import random
+import time
+start_time = time.time()
 
-
+# not in use
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
     v = math.factorial(v)
@@ -9,12 +13,27 @@ def slowfun_too_slow(x, y):
 
     return v
 
+#fill the cache with the possible values
+cache = {}
+def populate_cache(x1, x2, y1, y2):
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            v = math.pow(x, y)
+            v = math.factorial(v)
+            v //= (x + y)
+            v %= 982451653
+            cache[f"{x}_{y}"] = v
+#set the range
+populate_cache(2, 14, 3, 6)
+
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    return cache[f"{x}_{y}"]
+               
 
 
 
@@ -24,3 +43,5 @@ for i in range(50000):
     x = random.randrange(2, 14)
     y = random.randrange(3, 6)
     print(f'{i}: {x},{y}: {slowfun(x, y)}')
+
+print("Script complete in %.2f seconds." % (time.time() - start_time))
