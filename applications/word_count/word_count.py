2355e2449509d70f03843768453241f66b83c484
Michael-B1764 <mikebailar@gmail.com>
2020-08-05 14:05:29 -0700

    finished word_count 3/8

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,5 +1,14 @@
+
 def word_count(s):
     # Your code here
+    s_stripped = s.replace('"', '').replace(':', '').replace(';', '').replace(',', '').replace('.', '').replace('-', '').replace('+', '').replace('=', '').replace('/', '').replace('\\', '').replace("|", '').replace('[', '').replace(']', '').replace('{', '').replace('}', '').replace('(', '').replace(')', '').replace('*', '').replace('^', '').replace('&', '')
+    if s_stripped == "":
+        return {}
+    else:
+        s_split = s_stripped.lower().split()
+        s_word_count = {w:s_split.count(w) for w in s_split}
+        print(s_word_count)
+        return s_word_count
 
 
 
@@ -8,3 +17,4 @@ if __name__ == "__main__":
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
     print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('":;,.-+=/\\|[]{}()*^&'))
